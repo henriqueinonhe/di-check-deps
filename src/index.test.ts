555c358d490d0a3624d07a7c8dbd0a19be98b97c
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2022-08-15 20:22:31 -0300

    Add tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,57 +1,248 @@
 import { checkDependencies } from "./index";
 
+// Setup
+
 const makeA = () => ({
-  x: 10,
+  x: 234234,
 });
 
 type A = ReturnType<typeof makeA>;
 
-const makeB = ({ a }: { a: A }) => ({
-  y: "Duba",
-});
+type BDeps = {
+  a: A;
+};
+
+const makeB =
+  ({ a }: BDeps) =>
+  () => ({
+    y: "asdasdas",
+  });
 
 type B = ReturnType<typeof makeB>;
 
-const makeC = ({ b }: { b: B }) => ({
-  z: true,
-});
+type CDeps = {
+  a: A;
+  b: B;
+};
+
+const makeC =
+  ({ a, b }: CDeps) =>
+  () => ({
+    z: true,
+  });
+
+type C = ReturnType<typeof makeC>;
+
+type DDeps = {
+  a: A;
+  c: C;
+};
 
 class D {
-  constructor({ a }: { a: A }) {}
+  constructor({ a, c }: DDeps) {}
 }
 
-const makeE = ({ val }: { val: number }) => undefined;
+type EDeps = {
+  val: number;
+};
 
-checkDependencies({
-  a: makeA,
-  b: makeB,
-  c: makeC,
-});
+const makeE = ({ val }: EDeps) => null;
 
-checkDependencies({
-  b: makeB,
-});
+type F = () => { w: null };
 
-checkDependencies({
-  c: makeC,
-});
+type FDeps = {
+  f: F;
+};
 
-checkDependencies({
-  b: makeB,
-  c: makeC,
-});
+const makeF =
+  ({ f }: FDeps): F =>
+  () => ({ w: null });
 
-checkDependencies({
-  a: () => "1",
-  b: makeB,
-  c: makeC,
-});
+type GDeps = {
+  h: H
+}
 
-checkDependencies({
-  d: D,
-});
+const makeG = ({}: GDeps) => () => () => true;
 
-checkDependencies({
-  val: 234234,
-  e: makeE,
-});
+type G = ReturnType<typeof makeG>
+
+type HDeps = {
+  g: G
+};
+
+const makeH = ({}: HDeps) => () => () => false
+
+type H = ReturnType<typeof makeH>;
+
+// Tests
+
+// When we have a single service without any dependencies
+() => {
+  () => {
+    // It passes the check
+    checkDependencies({
+      a: makeA,
+    });
+  };
+};
+
+// When we have a service that depends on another service
+() => {
+  // And we do not provide that service
+  () => {
+    // It fails the check
+    () => {
+      // @ts-expect-error
+      checkDependencies({
+        b: makeB,
+      });
+    };
+  };
+
+  // And we DO provide that service
+  () => {
+    // It passes the check
+    () => {
+      checkDependencies({
+        a: makeA,
+        b: makeB,
+      });
+    };
+  };
+
+  // And we DO provide that service but with
+  // the wrong name
+  () => {
+    // It fails the check
+    () => {
+      //@ts-expect-error
+      checkDependencies({
+        aDep: makeA,
+        b: makeB,
+      });
+    };
+  };
+};
+
+// When we have a service that has more than one
+// dependency
+() => {
+  // And we do not provide them
+  () => {
+    // It fails the check
+    () => {
+      //@ts-expect-error
+      checkDependencies({
+        c: makeC,
+      });
+
+      //@ts-expect-error
+      checkDependencies({
+        a: makeA,
+        c: makeC,
+      });
+
+      //@ts-expect-error
+      checkDependencies({
+        b: makeB,
+        c: makeC,
+      });
+    };
+  };
+
+  // And we provide all of them
+  () => {
+    // It passes the check
+    () => {
+      checkDependencies({
+        a: makeA,
+        b: makeB,
+        c: makeC,
+      });
+    };
+  };
+};
+
+// When a service is created using a class resolver
+() => {
+  // And some of its dependencies are missing
+  () => {
+    // It fails the check
+    () => {
+      //@ts-expect-error
+      checkDependencies({
+        d: D,
+      });
+
+      //@ts-expect-error
+      checkDependencies({
+        a: makeA,
+        d: D,
+      });
+    };
+  };
+
+  // And all of its dependencies are provided
+  () => {
+    // It passes the check
+    () => {
+      checkDependencies({
+        a: makeA,
+        b: makeB,
+        c: makeC,
+        d: D,
+      });
+    };
+  };
+};
+
+// When a service is provided using a value resolver
+() => {
+  // It passes the check
+  () => {
+    checkDependencies({
+      val: 1234,
+      e: makeE,
+    });
+  };
+};
+
+// When a service has a circular dependency on itself
+() => {
+  // It passes the check
+  () => {
+    checkDependencies({
+      f: makeF,
+    });
+  };
+};
+
+// When a service has an indirect circular dependency 
+() => {
+  // And all dependencies are provided
+  () => {
+    // It passes the check
+    () => {
+      checkDependencies({
+        g: makeG,
+        h: makeH
+      })
+    }
+  }
+
+  // And NOT all dependencies are provided
+  () => {
+    // It fails the check
+    () => {
+      // @ts-expect-error
+      checkDependencies({
+        g: makeG,
+      });
+
+      // @ts-expect-error
+      checkDependencies({
+        h: makeH,
+      });
+    }
+  }
+}
